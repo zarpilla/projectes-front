88f57c252e330d3db1873a1c4b080dbda61148ca
Jordi Sabaté <jordi@webcoop.cat>
2021-06-01 18:00:30 +0200

    projectState fix?

--- src/components/DedicationPivot.vue
+++ src/components/DedicationPivot.vue
@@ -78,7 +78,7 @@ export default {
       const projectState = this.projectState !== null ? this.projectState : 1
       let query = `projects?_where[project_state_eq]=${projectState}`
       // let query = `activities?_where[date_gte]=${from}&[date_lte]=${to}`
-      if (projectState === 0) {
+      if (projectState === 0 || projectState === '0') {
         query = 'projects?_limit=999'
       }
 
