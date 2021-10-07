efe851273c0ff50720f58ddec4b8b5be78a3a4d5
Jordi <zarpa37@gmail.com>
2021-10-07 08:18:51 +0200

    list style

--- src/views/Documentation.vue
+++ src/views/Documentation.vue
@@ -4,12 +4,12 @@
     <section class="section is-main-section">
       <card-component title="Vídeos">
           <ul>
-              <li><a href="/stats/docum/01.intro.mkv" target="_blank">01. Introducció</a></li>
-              <li><a href="/stats/docum/02.01.projectes.mkv" target="_blank">02.01. Projectes</a></li>
-              <li><a href="/stats/docum/02.02.projectes-contactes.mkv" target="_blank">02.02. Projectes: Contactes</a></li>
-              <li><a href="/stats/docum/02.03.projectes-ingressos-despeses-fases-planificacio.mkv" target="_blank">02.03. Projectes: Fases i pressupost intern</a></li>
-              <li><a href="/stats/docum/03.hores-entrada.mkv" target="_blank">03. Entrada d'hores dedicades</a></li>
-              <li><a href="/stats/docum/03.hores-saldo.mkv" target="_blank">04. Saldo d'hores</a></li>
+              <li><a href="/stats/docum/01.intro.mkv" target="_blank">Introducció</a></li>
+              <li><a href="/stats/docum/02.01.projectes.mkv" target="_blank">Projectes</a></li>
+              <li><a href="/stats/docum/02.02.projectes-contactes.mkv" target="_blank">Projectes: Contactes</a></li>
+              <li><a href="/stats/docum/02.03.projectes-ingressos-despeses-fases-planificacio.mkv" target="_blank">Projectes: Fases i pressupost intern</a></li>
+              <li><a href="/stats/docum/03.hores-entrada.mkv" target="_blank">Entrada d'hores dedicades</a></li>
+              <li><a href="/stats/docum/03.hores-saldo.mkv" target="_blank">Saldo d'hores</a></li>
           </ul>
       </card-component>
     </section>
@@ -31,3 +31,9 @@ export default {
   }
 }
 </script>
+<style scoped>
+ul{
+    list-style: disc;
+    margin-left: 0.5rem;
+}
+</style>
