88fc78b0817b3e1c3bf9d3821ec3740aa17654c7
Jordi <zarpa37@gmail.com>
2021-10-11 17:35:59 +0200

    sw version

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -5,7 +5,7 @@ import { register } from 'register-service-worker'
 if (process.env.NODE_ENV === 'production') {
   register(`${process.env.BASE_URL}service-worker.js`, {
     ready () {
-      console.log('projectes v.0.9.1')
+      console.log('projectes v.0.9.2')
       console.log(
         'App is being served from cache by a service worker.\n' +
         'For more details, visit https://goo.gl/AFskqB'
