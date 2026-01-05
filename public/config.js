f1b555c9723108ab43ad16bf75c618acfe8b3231
Jordi Sabaté <3697530+zarpilla@users.noreply.github.com>
2026-01-05 16:00:16 +0100

    generic data

--- public/config.js
+++ public/config.js
@@ -1,6 +1,6 @@
 // Development configuration - fallback values
 window.APP_CONFIG = {
-  VUE_APP_API_URL: 'https://projectes.arrandeterra.org',
+  VUE_APP_API_URL: 'http://127.0.0.1:1337',
   VUE_APP_RESET_PASSWORD: 'http://localhost:8080/#/reset-password',
-  VUE_APP_PATH: 'https://projectes.arrandeterra.org/stats/'
+  VUE_APP_PATH: 'http://localhost:8080/#/stats/'
 };
